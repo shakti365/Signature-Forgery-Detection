fbaf37a9f1eb823ab9084ea12d4482ff0476492c
Shivam Shakti <shivam.shakti365@gmail.com>
2018-11-19 01:35:51 +0530

    Add tensorboard and tensorflow serving support. Fix model strcuture.

--- src/model/tf_model.py
+++ src/model/tf_model.py
@@ -1,27 +1,47 @@
 import tensorflow as tf
 import numpy as np
 import os
+import utils
 
 
 class SiameseCNN:
 
-    def __init__(self):
+    def __init__(self, config):
 
-        self.data_path = '../../data/processed/'
-        self.train_batch_size = 8
-        self.valid_batch_size = 8
-        self.seed = 42
-        self.learning_rate=0.001
-        self.epochs = 1
+        self.data_path = config['data_path']
+        self.train_batch_size = config['valid_batch_size']
+        self.valid_batch_size = config['train_batch_size']
+        self.seed = config['seed']
+        self.learning_rate=config['learning_rate']
+        self.epochs = config['epochs']
+        self.export_dir = os.path.join(config['export_dir'], config['model_name'])
+        self.model_name = config['model_name']
+        self.SERVING_DIR=os.path.join(self.export_dir, self.model_name+'_serving', '1')
+        self.TF_SUMMARY_DIR=os.path.join(self.export_dir, self.model_name+'_summary')
+        self.CKPT_DIR=os.path.join(self.export_dir, self.model_name+'_checkpoint')
+        self.log_step=config['log_step']                
 
-    def numpy_input_fn(self, train_X_1, train_X_2, train_X_3, train_y, valid_X_1, valid_X_2, valid_X_3, valid_y):
+
+    def numpy_input_fn(self):
         """
 
         """
     
+        # Get train set
+        train = np.load(os.path.join(self.data_path, 'train.npz'))
+        X_1_train=train['X_1_train'].astype(np.float32)
+        X_2_train=train['X_2_train'].astype(np.float32)
+        X_3_train=train['X_3_train'].astype(np.float32)
+
+        # Get valid set
+        valid = np.load(os.path.join(self.data_path, 'valid.npz'))
+        X_1_valid=valid['X_1_valid'].astype(np.float32)
+        X_2_valid=valid['X_2_valid'].astype(np.float32)
+        X_3_valid=valid['X_3_valid'].astype(np.float32)
+
         # Create Dataset object from input.
-        train_dataset = tf.data.Dataset.from_tensor_slices((train_X_1, train_X_2, train_X_3, train_y)).batch(self.train_batch_size)
-        valid_dataset = tf.data.Dataset.from_tensor_slices((valid_X_1, valid_X_2, valid_X_3, valid_y)).batch(self.valid_batch_size)
+        train_dataset = tf.data.Dataset.from_tensor_slices((X_1_train, X_2_train, X_3_train)).batch(self.train_batch_size)
+        valid_dataset = tf.data.Dataset.from_tensor_slices((X_1_valid, X_2_valid, X_3_valid)).batch(self.valid_batch_size)
         
         # Create generic iterator.
         data_iter = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
@@ -33,30 +53,30 @@ class SiameseCNN:
         return train_init_op, valid_init_op, data_iter
 
 
-    def model(self, x, reuse=False):
+    def model(self, x):
         """
 
         """
 
-        with tf.variable_scope("siamese_cnn", reuse=reuse):
+        with tf.variable_scope("signet", reuse=tf.AUTO_REUSE):
             conv1 = tf.layers.conv2d(inputs=x, filters=96, kernel_size=[11,11], activation=tf.nn.relu) 
             norm1 = tf.nn.local_response_normalization(conv1)
             pool1 = tf.layers.max_pooling2d(inputs=norm1, pool_size=[3,3], strides=2)
             pad1 = tf.pad(pool1, paddings=tf.constant([[0, 0], [2,2], [2,2], [0,0]]))
 
-            conv2 = tf.layers.conv2d(inputs=pad1, filters=256, kernel_size=[5,5]) 
+            conv2 = tf.layers.conv2d(inputs=pad1, filters=256, kernel_size=[5,5], activation=tf.nn.relu) 
             norm2 = tf.nn.local_response_normalization(conv2) 
             pool2 = tf.layers.max_pooling2d(inputs=norm2, pool_size=[3,3], strides=2)
 
 
-            conv3 = tf.layers.conv2d(inputs=pool2, filters=384, kernel_size=[3,3]) 
-            conv4 = tf.layers.conv2d(inputs=conv3, filters=256, kernel_size=[3,3]) 
+            conv3 = tf.layers.conv2d(inputs=pool2, filters=384, kernel_size=[3,3], activation=tf.nn.relu) 
+            conv4 = tf.layers.conv2d(inputs=conv3, filters=256, kernel_size=[3,3], activation=tf.nn.relu) 
             pool3 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[3,3], strides=2)
 
             shapes = pool3.get_shape()
             pool3_flattened = tf.reshape(pool3, [-1, shapes[1] * shapes[2] * shapes[3]])
-            dense1 = tf.layers.dense(inputs=pool3_flattened, units=1024)
-            logits = tf.layers.dense(inputs=dense1, units=128)
+            dense1 = tf.layers.dense(inputs=pool3_flattened, units=1024, activation=tf.nn.relu)
+            logits = tf.layers.dense(inputs=dense1, units=128, activation=tf.nn.relu)
 
         return logits
 
@@ -64,82 +84,239 @@ class SiameseCNN:
     def euclidean_distance(self, y_true, y_pred):
         """
         """
-        euclidean_norm = tf.math.reduce_sum(tf.math.squared_difference(y_pred, y_true), axis=-1)
+        with tf.name_scope("euclidean_norm"):
+            euclidean_norm = tf.math.reduce_sum(tf.math.squared_difference(y_pred, y_true), axis=-1)
         return euclidean_norm
 
-    def triplet_loss(self, embeddings):
+    def triplet_loss(self, anchor, positive, negative):
         """
         """
+        with tf.name_scope("triplet_loss"):
+            positive_dist= self.infer(anchor, positive)
+            negative_dist = self.infer(anchor, negative)
 
-        processed_a, processed_p, processed_n = embeddings[0], embeddings[1], embeddings[2]
+            margin = 0.05
+            triplet_loss_op = tf.math.maximum(0.0, margin + positive_dist - negative_dist)
 
-        positive_dist= self.euclidean_distance(processed_a, processed_p)
-        negative_dist = self.euclidean_distance(processed_a, processed_n)
-
-        margin = 0.05
-        triplet_loss_op = tf.math.maximum(0.0, margin + positive_dist - negative_dist)
-
-        loss =  tf.math.reduce_mean(triplet_loss_op)
+            loss =  tf.math.reduce_mean(triplet_loss_op)
         return loss
 
 
-    def train(self, x1, x2, x3, y):
+    def train(self, x1, x2, x3):
         """
         
         """
-        embedding_x1 = self.model(x1) 
-        embedding_x2 = self.model(x2, reuse=True) 
-        embedding_x3 = self.model(x3, reuse=True)
-        loss_op = self.triplet_loss([embedding_x1, embedding_x2, embedding_x3])
-        train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(loss_op) 
-        return loss_op, train_op
+        with tf.name_scope("train"):
+            loss_op = self.triplet_loss(x1, x2, x3)
+            train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(loss_op) 
+
+            summary_op, metrics_update_op = utils.get_metrics(loss_op)
+
+        return train_op, summary_op, metrics_update_op
 
 
+    def infer(self, x1, x2, reuse=tf.AUTO_REUSE):
+        """
+
+        """
+        with tf.variable_scope("siamese_network", reuse=reuse) as scope:
+            embedding_x1 = self.model(x1) 
+            scope.reuse_variables()
+            embedding_x2 = self.model(x2) 
+
+        embedding_dist = self.euclidean_distance(embedding_x1, embedding_x2)
+
+        return embedding_dist
 
     def fit(self):
+
+        # Check if the export directory is present,
+        # if not present create new directory.
+        if os.path.exists(self.export_dir):
+            raise ValueError("Export directory already exists. Please specify different export directory.")
+        else:
+            os.mkdir(self.export_dir)
+
+
+        self.builder=tf.saved_model.builder.SavedModelBuilder(self.SERVING_DIR)
+
 
         tf.reset_default_graph()
         tf.set_random_seed(self.seed)
 
-        # Get train set
-        train = np.load(os.path.join(self.data_path, 'train.npz'))
-        X_1_train=train['X_1_train'].astype(np.float32)
-        X_2_train=train['X_2_train'].astype(np.float32)
-        X_3_train=train['X_3_train'].astype(np.float32)
-        y_train=train['y_train'].astype(np.float32)
+        train_init_op, valid_init_op, data_iter = self.numpy_input_fn()
+        x1, x2, x3 = data_iter.get_next()
 
-        # Get valid set
-        valid = np.load(os.path.join(self.data_path, 'valid.npz'))
-        X_1_valid=valid['X_1_valid'].astype(np.float32)
-        X_2_valid=valid['X_2_valid'].astype(np.float32)
-        X_3_valid=valid['X_3_valid'].astype(np.float32)
-        y_valid=valid['y_valid'].astype(np.float32)
+        train_op, summary_op, metrics_update_op = self.train(x1, x2, x3)
 
-        train_init_op, valid_init_op, data_iter = self.numpy_input_fn(X_1_train, X_2_train, X_3_train, y_train, X_1_valid, X_2_valid, X_3_valid, y_valid)
-        X1, X2, X3, Y = data_iter.get_next()
+        prediction = self.infer(x1, x2)
 
-        loss_op, train_op = self.train(X1, X2, X3, Y)
+        # Ops to reset metrics at the end of every epoch
+        metrics_global_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='metrics')
+        metrics_local_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope='metrics')
+        metrics_vars = metrics_global_vars + metrics_local_vars
+        metrics_init_op = tf.variables_initializer(var_list=metrics_vars)
+
+        # Object to saver model checkpoints
+        self.saver = tf.train.Saver()
         
         with tf.Session() as sess:
 
             sess.run(tf.global_variables_initializer())
             sess.run(tf.local_variables_initializer())
-            
+
+            # Create file writer directory to store summary and events.
+            train_writer = tf.summary.FileWriter(self.TF_SUMMARY_DIR+'/train', sess.graph)
+            valid_writer = tf.summary.FileWriter(self.TF_SUMMARY_DIR+'/valid')
+
             for epoch in range(self.epochs):
 
                 sess.run(train_init_op)
 
                 while True:
                     try:
-                        loss, _ = sess.run([loss_op, train_op])
-                        print (epoch, loss)
+                        train_summary,_,_ = sess.run([summary_op, train_op, metrics_update_op])
+                        break
                     except tf.errors.OutOfRangeError:
                         break
 
+                if epoch % self.log_step == 0:
 
+                    train_writer.add_summary(train_summary, epoch)
+
+                    # Save model checkpoint.
+                    self.saver.save(sess, self.CKPT_DIR+"{}.ckpt".format(self.model_name))
+                    
+                    sess.run(valid_init_op)
+
+                    while True:
+                        try:
+                            valid_summary,_ = sess.run([summary_op, metrics_update_op])
+                            valid_writer.add_summary(valid_summary, epoch)
+                            break
+                        except tf.errors.OutOfRangeError:
+                            break
+                    
+
+            prediction_signature = self.create_prediction_signature(x1, x2, prediction)
+            self.save_servables(prediction_signature, signature_def_key='predictions')
+
+
+    def predict(self, x1, x2):
+        """
+        """ 
+
+        tf.reset_default_graph()
+        
+        x1_ = tf.placeholder(shape=[None, 155, 220, 1], dtype=tf.float32, name='input')
+        x2_ = tf.placeholder(shape=[None, 155, 220, 1], dtype=tf.float32, name='input')
+
+        embedding_dist = self.infer(x1_, x2_, reuse=True)
+
+        self.saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+
+            self.saver.restore(sess, self.CKPT_DIR+"{}.ckpt".format(self.model_name))
+
+            dist = sess.run(embedding_dist, {x1_:x1, x2_:x2})
+
+        return dist
+
+
+    def create_prediction_signature(self, x1, x2, prediction):
+        """
+        Creates prediction signature for tensorflow serving
+        
+        Returns
+        -------
+            prediction_signature: 
+                Prediction signature with input and output defined 
+                for model for setting up tensorflow serving.
+                
+        """
+        # Create input and output utils for prediction signature.
+        tensor_info_x1 = tf.saved_model.utils.build_tensor_info(x1)
+        tensor_info_x2 = tf.saved_model.utils.build_tensor_info(x1)
+        
+        tensor_info_prediction = tf.saved_model.utils.build_tensor_info(prediction)
+
+        # Create prediction signature.
+        prediction_signature = (
+            tf.saved_model.signature_def_utils.build_signature_def(
+                method_name = tf.saved_model.signature_constants.PREDICT_METHOD_NAME,
+                inputs = {
+                    'x1': tensor_info_x1,
+                    'x2': tensor_info_x2
+                },
+                outputs = {
+                    'logits': tensor_info_prediction
+                },
+            )
+        )
+        
+        return prediction_signature
+
+
+    def save_servables(self, prediction_signature, signature_def_key):
+        """
+        Loads the latest model checkpoint and saves model as a servable for production
+        
+        Parameters
+        ----------
+            prediction_signature: 
+                Prediction signature with input and output defined 
+                for model for setting up tensorflow serving.
+            
+            signature_def_key: string
+                Specify a user-generated-key for SignatureDef, 
+                this key is used to map Signature Definition.
+        """
+        with tf.Session() as sess:
+            # Restore model checkpoint.
+            self.saver.restore(sess, self.CKPT_DIR+"{}.ckpt".format(self.model_name))
+            
+            # Map signature defnition with a specified key.
+            signature_def_map = {
+                signature_def_key: prediction_signature
+            }
+            
+            # Tags for meta-graph.
+            tags = [tf.saved_model.tag_constants.SERVING]
+            
+            # Saving tensorflow servables.
+            self.builder.add_meta_graph_and_variables(
+                sess = sess,
+                tags = tags,
+                signature_def_map = signature_def_map
+            )
+            self.builder.save()
 
 
 if __name__=="__main__":
-    
-    siamese_model = SiameseCNN()
+
+    config = dict()
+
+    config['data_path'] = '../../data/processed'
+    config['valid_batch_size'] = 8
+    config['train_batch_size'] = 8
+    config['seed'] = 42
+    config['learning_rate'] = 0.001
+    config['epochs'] = 1
+    config['export_dir'] = '../../data/models'
+    config['model_name'] = 'exp_4'
+    config['log_step'] = 1
+
+    siamese_model = SiameseCNN(config)
     siamese_model.fit()
+
+    # Get test set
+    test = np.load(os.path.join(config['data_path'], 'test.npz'))
+    X_1_test=test['X_1_test'].astype(np.float32)[:5]
+    X_2_test=test['X_2_test'].astype(np.float32)[:5]
+    X_3_test=test['X_3_test'].astype(np.float32)[:5]
+
+    pos = siamese_model.predict(X_1_test, X_2_test)
+    neg = siamese_model.predict(X_1_test, X_3_test)
+
+    print (pos, neg)
---
+++ src/model/utils.py
@@ -0,0 +1,143 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, roc_curve, roc_auc_score, f1_score, accuracy_score
+
+def get_metrics(loss):
+    """
+    Return accuracy, precision, recall and f1_score metrics
+
+    Parameters
+    ----------
+        labels: Tensor shape=[batch_size, 1]
+            true labels.
+        predicted_proba: Tensor shape=[batch_size, 1]
+            predicted probabilities.
+        threshold: float
+            threshold used for predicted probabilties.
+        loss: Tensor shape=[1]
+            Weighted mean of loss over every sample in batch.
+    
+    Returns
+    -------
+        merged: Tensor
+            Tensor Op that merges all the tensorboard summaries for logging.
+        grouped_update_op: Tensor
+            Tensor Op which merges all the metrics updated ops into one.
+    """
+
+    with tf.variable_scope('metrics'):
+
+        # create variable for storing sum and count of loss to calculate mean over batch. 
+        total = tf.get_variable(initializer=0.0, dtype=tf.float32, name='total_loss', trainable=False)
+        count = tf.get_variable(initializer=0.0, dtype=tf.float32, name='count_loss', trainable=False)
+	
+	
+        with tf.name_scope('mean_loss'):
+            update_total = tf.assign_add(total, loss)
+            update_count = tf.assign_add(count, 1.0)
+            mean_loss = tf.divide(total, count)
+            mean_loss_update = tf.group([update_total, update_count])
+	
+	tf.summary.scalar('loss', loss)
+        tf.summary.scalar('mean_loss', mean_loss)
+	
+        # merge all summary as an Op.
+	merged = tf.summary.merge_all()
+
+			       
+    return merged, mean_loss_update
+
+
+def display_metrics(y, logits, threshold):
+    """
+    Log metrics for binary classification on console output.
+    
+    Parameters
+    ----------
+        y: numpy array shape=[num_samples, 1]
+            True labels for the samples.
+        
+        logits: numpy array shape=[num_samples, 1]
+            Predicted probabilities for the samples.
+        
+        threshold: float
+            Threshold for classification.
+    """
+    # reshape results for train
+    y = y.flatten()
+    logits = logits.flatten()
+    y_ = np.where(logits>threshold, 1, 0)
+    
+    confusion = confusion_matrix(y_true=y, y_pred=y_)
+    accuracy = accuracy_score(y_true=y, y_pred=y_)
+    precision = precision_score(y_true=y, y_pred=y_)
+    recall = recall_score(y_true=y, y_pred=y_)
+    auc = roc_auc_score(y, logits)
+    f1 = f1_score(y, y_)
+    
+    print ("confusion matrix: \n", confusion)
+    print ("accuracy score: ", accuracy) 
+    print ("precision score: ", precision)
+    print ("recall score: ", recall)
+    print ("F1 score: ", f1)
+    print ("AUC: ", auc)
+
+
+
+
+
+def variable_summaries(var, var_name=None):
+    """
+    Creates summaries of a tensor variable.
+
+    Parameters
+    ----------
+        var: Tensor
+             A tensor variable for which the summaries have to be created.
+        var_name: String (default None)
+             Name with which the summaries are logged.
+             If None, it takes the name of the variable.
+    """
+    if var_name is None:
+        var_name= var.name
+
+    mean = tf.reduce_mean(var)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('{}_mean'.format(var_name), mean)
+    tf.summary.scalar('{}_stddev'.format(var_name), stddev)
+    tf.summary.histogram('{}_histogram'.format(var_name), var)
+
+def gradient_summaries(gradients):
+    """
+    Creates gradient summaries.
+
+    Parameters
+    ----------
+        gradients: List of Tuple [(Tensor,Tensor)]
+            A list of tuples of tensor varible and values.
+    """
+    with tf.name_scope('gradients'):
+        # Save the gradients summary for tensorboard.
+        for grad in gradients:
+	    # Assign a name to identify gradients.
+	    var_name = '{}-grad'.format(grad[1].name)
+	    if 'bias' not in var_name:
+	        variable_summaries(grad[0], var_name=var_name)
+
+def parameter_summaries(variables):
+    """
+    Creates parameter summaries.
+
+    Parameter
+    ---------
+        variables: List of Tensor
+            A list of trainable parameters in the model.
+    """
+    with tf.name_scope('variables'):
+        # Save the gradients summary for tensorboard.
+        for var in variables:
+	    # Assign a name to identify gradients.
+	    var_name = '{}-variable'.format(var.name)
+	    if 'bias' not in var_name:
+	        variable_summaries(var, var_name=var_name)
+
