920ad179c98c5caf42d001b497d4021dcfce3099
Shivam Shakti <shivam.shakti365@gmail.com>
2018-11-28 03:20:08 +0530

    Add LIME explanations for signatures

--- src/serving/signature.py
+++ src/serving/signature.py
@@ -19,6 +19,14 @@ bp = Blueprint('signature', __name__, url_prefix='/signature')
 UPLOAD_PATH='uploads'
 upload_folder = os.path.abspath('serving/static')
 
+import matplotlib.pyplot as plt
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+
+import numpy as np
+import scipy.misc
+
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -95,7 +103,38 @@ def verify():
             x1 = x[0].tolist()
             x2 = x[1].tolist()
 
-            payload = {"signature_name":"predictions", "instances": [{"x1": x1, "x2":x2}]}
+            output = predict_forgery(x1, x2)
+
+            explanation = explain(x[0], x[1], test_image.filename)
+
+            return render_template('result.html', 
+                                    output=output['predictions'][0], 
+                                    filename1=real_signature_path, 
+                                    filename2=test_signature_path, 
+                                    explanation=explanation)
+
+        flash(error)
+
+    return render_template('verify.html')
+
+
+def explain(x2, x1, filename):
+        
+    explainer = lime_image.LimeImageExplainer()
+    def predict_forgery(x1):
+        if len(x1.shape) == 3:
+            x1 = np.expand_dims(x1, axis=0)
+
+        results = []
+        for idx in range(x1.shape[0]):
+
+            x_select = x1[idx]
+
+            if x_select.shape[-1] == 3:
+                x_select = x_select[:, :, :1]
+
+            x = x_select.tolist()
+            payload = {"signature_name":"predictions", "instances": [{"x1": x, "x2":x2.tolist()}]}
 
             r = requests.post('http://localhost:8501/v1/models/my_model:predict', json=payload)
 
@@ -103,12 +142,32 @@ def verify():
                 output = r.json()
             else:
                 output = "Error!"
+            result = output['predictions'][0]
+            results.append([result, 0])
 
-            return render_template('result.html', 
-                                    output=output['predictions'][0], 
-                                    filename1=real_signature_path, 
-                                    filename2=test_signature_path)
+        return np.array(results, ndmin=2)
+    
+    
+    explanation = explainer.explain_instance(x1[:, :, 0], predict_forgery, labels=[1], num_samples=5)
+    temp, mask = explanation.get_image_and_mask(0, positive_only=True, num_features=10, hide_rest=False)
+    explanation = mark_boundaries(temp / 2 + 0.5, mask)
 
-        flash(error)
+    explanation_path = os.path.join(UPLOAD_PATH, 'explanation_{}.png'.format(filename))
 
-    return render_template('verify.html')
+    scipy.misc.imsave(os.path.join(upload_folder, explanation_path), explanation)
+    
+    return explanation_path
+
+
+def predict_forgery(x1, x2):
+
+    payload = {"signature_name":"predictions", "instances": [{"x1": x1, "x2":x2}]}
+
+    r = requests.post('http://localhost:8501/v1/models/my_model:predict', json=payload)
+
+    if r.status_code == 200:
+        output = r.json()
+    else:
+        output = "Error!"
+
+    return output
